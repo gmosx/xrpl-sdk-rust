72dcd19ad91480be9a5bd668b3ebd84119166295
George Moschovitis <gmosx@reizu.org>
2022-10-28 09:11:02 +0300

    Minor edits

--- xrpl_api/src/api/ledger.rs
+++ xrpl_api/src/api/ledger.rs
@@ -82,48 +82,48 @@ impl LedgerExpandRequest {
 
 #[derive(Debug, Deserialize)]
 pub struct LedgerResponse<TransactionType> {
-    ///Unique identifying hash of the entire ledger.
+    /// Unique identifying hash of the entire ledger.
     pub ledger_hash: String,
-    ///The Ledger Index of this ledger.
+    /// The Ledger Index of this ledger.
     pub ledger_index: u32,
-    ///(May be omitted) If true, this is a validated ledger version. If omitted or set to false, this ledger's data is not final.
+    /// (May be omitted) If true, this is a validated ledger version. If omitted or set to false, this ledger's data is not final.
     pub validated: bool,
-    ///(Omitted unless requested with the queue parameter) Array of objects describing queued transactions, in the same order as the queue.
-    ///If the request specified expand as true, members contain full representations of the transactions, in either JSON or binary depending on whether the request specified binary as true.
+    /// (Omitted unless requested with the queue parameter) Array of objects describing queued transactions, in the same order as the queue.
+    /// If the request specified expand as true, members contain full representations of the transactions, in either JSON or binary depending on whether the request specified binary as true.
     pub queue_data: Option<u64>,
-    ///The complete header data of this ledger.
+    /// The complete header data of this ledger.
     pub ledger: Ledger<TransactionType>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Ledger<TransactionType> {
-    ///Hash of all account state information in this ledger, as hex.
+    /// Hash of all account state information in this ledger, as hex.
     pub account_hash: String,
     pub close_flags: u64,
-    ///The time this ledger was closed, in seconds since the Ripple Epoch.
+    /// The time this ledger was closed, in seconds since the Ripple Epoch.
     pub close_time: u64,
-    ///The time this ledger was closed, in human-readable format. Always uses the UTC time zone.
+    /// The time this ledger was closed, in human-readable format. Always uses the UTC time zone.
     pub close_time_human: String,
-    ///Ledger close times are rounded to within this many seconds.
+    /// Ledger close times are rounded to within this many seconds.
     pub close_time_resolution: u64,
-    ///Whether or not this ledger has been closed.
+    /// Whether or not this ledger has been closed.
     pub closed: bool,
-    ///Unique identifying hash of the entire ledger.
+    /// Unique identifying hash of the entire ledger.
     pub ledger_hash: String,
-    ///The Ledger Index of this ledger, as a quoted integer.
+    /// The Ledger Index of this ledger, as a quoted integer.
     pub ledger_index: String,
-    ///The time at which the previous ledger was closed.
+    /// The time at which the previous ledger was closed.
     pub parent_close_time: u64,
-    ///Unique identifying hash of the ledger that came immediately before this one.
+    /// Unique identifying hash of the ledger that came immediately before this one.
     pub parent_hash: String,
-    ///Total number of XRP drops in the network, as a quoted integer. (This decreases as transaction costs destroy XRP.)
+    /// Total number of XRP drops in the network, as a quoted integer. (This decreases as transaction costs destroy XRP.)
     pub total_coins: String,
-    ///Hash of the transaction information included in this ledger, as hex
+    /// Hash of the transaction information included in this ledger, as hex
     pub transaction_hash: String,
-    ///(Omitted unless requested) Transactions applied in this ledger version.
-    ///By default, members are the transactions' identifying Hash strings. If the request specified expand as true,
-    ///members are full representations of the transactions instead,
-    ///in either JSON or binary depending on whether the request specified binary as true.
+    /// (Omitted unless requested) Transactions applied in this ledger version.
+    /// By default, members are the transactions' identifying Hash strings. If the request specified expand as true,
+    /// members are full representations of the transactions instead,
+    /// in either JSON or binary depending on whether the request specified binary as true.
     pub transactions: Option<Vec<TransactionType>>,
 }
 
