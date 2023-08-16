9ae4a727d58e4d1d9d5bd268e0c192ade9ef5766
Allan Brondum Rasmussen <allan@keyrock.eu>
2023-08-16 12:06:42 +0200

    fix

--- xrpl_api/src/types/transaction/variants/offer_create.rs
+++ xrpl_api/src/types/transaction/variants/offer_create.rs
@@ -1,6 +1,6 @@
 use crate::TransactionCommon;
 use serde::{Deserialize, Serialize};
-use xrpl_types::Amount;
+use xrpl_types::{Amount, LedgerTimestamp};
 
 /// An `OfferCreate` transaction <https://xrpl.org/offercreate.html>
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -8,8 +8,13 @@ use xrpl_types::Amount;
 pub struct OfferCreateTransaction {
     #[serde(flatten)]
     pub common: TransactionCommon,
-    pub expiration: Option<u32>,
+    pub expiration: Option<LedgerTimestamp>,
     pub offer_sequence: Option<u32>,
     pub taker_gets: Amount,
     pub taker_pays: Amount,
+
+    /// `owner_funds` is present in transactions returned by book subscription, see
+    /// <https://xrpl.org/subscribe.html#order-book-streams>.
+    #[serde(rename = "owner_funds")]
+    pub owner_funds: Option<String>,
 }
---
+++ xrpl_types/src/ledger_timestamp.rs
@@ -0,0 +1,2 @@
+/// Type that measures seconds since Ripple Epoch <https://xrpl.org/basic-data-types.html#specifying-time>
+pub type LedgerTimestamp = u32;
