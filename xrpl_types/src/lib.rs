e390eecaf25ad4ead9c084269a88485db55b898c
zees-dev <dev.786zshan@gmail.com>
2023-11-02 17:56:12 +1300

    xrpl_types: top-level lib support for no_Std

--- xrpl_types/src/lib.rs
+++ xrpl_types/src/lib.rs
@@ -1,6 +1,9 @@
 //! Core types and related functions for the XRP Ledger. Reused between Web and
 //! WebSocket clients and potentially for server-side code.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+extern crate alloc;
+
 mod error;
 pub mod serialize;
 /// Types in internal canonical binary format <https://xrpl.org/serialization.html#type-list>
